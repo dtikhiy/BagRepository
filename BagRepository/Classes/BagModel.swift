fc8b201d6eaecd4a2c068d13d7c028fbea78ded6
dtikhiy <dima.quites@gmail.com>
2018-10-19 11:17:56 -0700

    Added public init

--- BagRepository/Classes/BagModel.swift
+++ BagRepository/Classes/BagModel.swift
@@ -2,8 +2,13 @@ import Foundation
 
 public final class Bag {
     
-    let bagId = "123"
-    let bagCount = 5
+    var bagId: String
+    var bagCount: Int
+    
+    public init(bagId: String, bagCount: Int) {
+        self.bagId = bagId
+        self.bagCount = bagCount
+    }
     
     public func getBag() -> String {
         return "This is our bag"
